f2a4cf6aed009e4238d5ee36f15df27617763e1a
kokosanki <os_mo@wp.pl>
2021-01-10 21:17:53 +0100

    add createTaskModal

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,46 @@
 <template>
   <v-app>
-    <v-app-bar app color="deep-purple darken-4" dark>
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <img style="width: 50px" src="@/assets/cat-64.png" alt="logo">
-        <h1 class="pink--text text--accent-2">Kanbanya</h1>
+        <h1 class="secondary--text">Kanbanya</h1>
       </div>
       <v-spacer></v-spacer>
-      <v-btn color="pink accent-2" class="deep-purple--text text--darken-4 font-weight-black">Create a task</v-btn>
+      <v-btn
+      @click="openCreateTaskModal"
+      color="secondary"
+      class="primary--text font-weight-black">Create a task</v-btn>
     </v-app-bar>
 
     <v-main>
       <Home />
+      <create-task-modal
+      @close="closeCreateTaskModal"
+      :active="isCreateTaskModalOpen"/>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import Home from '@/views/Home'
+import createTaskModal from '@/components/createTaskModal'
 
 export default {
   name: 'App',
-
-  components: { Home },
-
+  components: { Home, createTaskModal },
   data: () => ({
-    //
-  })
+    isCreateTaskModalOpen: false
+  }),
+  methods: {
+    createTask () {
+      console.log('create')
+    },
+    openCreateTaskModal () {
+      this.isCreateTaskModalOpen = true
+    },
+    closeCreateTaskModal () {
+      this.isCreateTaskModalOpen = false
+    }
+  }
 }
 </script>
---
+++ client/src/components/createTaskModal.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-row justify="center">
+    <v-dialog
+      v-model="active"
+      persistent
+      max-width="600px"
+    >
+      <v-card>
+        <v-card-title>
+          <span class="headline">Create a new task</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="Task title"
+                  required
+                ></v-text-field>
+              </v-col>
+
+              <v-col cols="12">
+                <v-text-field
+                  label="Task description"
+                  required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="closeModal"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            color="primary"
+            text
+            @click="createTask"
+          >
+            Create
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    active: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    closeModal () {
+      this.$emit('close')
+    },
+    createTask () {
+      console.log('ok')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
