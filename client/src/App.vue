16534ab2e39ae8c8303e67281505e32fa8aa148c
kokosanki <os_mo@wp.pl>
2021-01-10 19:58:59 +0100

    header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center"></div>
+    <v-app-bar app color="deep-purple darken-4" dark>
+      <div class="d-flex align-center">
+        <img style="width: 50px" src="@/assets/cat-64.png" alt="logo">
+        <h1 class="pink--text text--accent-2">Kanbanya</h1>
+      </div>
     </v-app-bar>
 
     <v-main>
