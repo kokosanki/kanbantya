42079272379176ef1a5ad826b928a142da54cb30
kokosanki <os_mo@wp.pl>
2021-01-10 20:08:42 +0100

    add create task button

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,6 +5,8 @@
         <img style="width: 50px" src="@/assets/cat-64.png" alt="logo">
         <h1 class="pink--text text--accent-2">Kanbanya</h1>
       </div>
+      <v-spacer></v-spacer>
+      <v-btn color="pink accent-2" class="deep-purple--text text--darken-4 font-weight-black">Create a task</v-btn>
     </v-app-bar>
 
     <v-main>
