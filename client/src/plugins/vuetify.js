65e9af141bc5fde12f733ea01e223ebf987787df
kokosanki <os_mo@wp.pl>
2021-01-10 21:15:45 +0100

    change vuetify default theme

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -4,4 +4,15 @@ import Vuetify from 'vuetify/lib/framework'
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme: {
+    light: true,
+    themes: {
+      light: {
+        primary: '#311b92',
+        secondary: '#ff4081',
+        accent: '#8c9eff',
+        error: '#b71c1c'
+      }
+    }
+  }
 })
